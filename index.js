c1da40a563dbb5d4ad5ab1b72ed0f6b1f9c236a7
Brad R <brad.w.rollinson@gmail.com>
2021-08-08 13:07:58 -0400

    create list/choice prompt for license

--- index.js
+++ index.js
@@ -36,11 +36,31 @@ const promptUser = () => {
             name: 'test',
             message: 'What are the test instructions for your README?',
         },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which license would you like to use for your README?',
+            choices: ['MIT', 'Mozilla', 'GNU']
+        },
     ]);
 };
 
+// const licenseBadge = '';
+
+// generateBadge = (answers) => {
+//     if (answers.license === 'MIT') {
+//         const licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+//     } else if (answers.license === 'Mozilla') {
+//         const licenseBadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+//     } else {
+//         const licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)';
+//     }
+// }
+
 const generateREADME = (answers) =>
     `# ${answers.title}
+
+${answers.license}
 
 ## Description:
 
@@ -64,6 +84,7 @@ ${answers.test}`
 
 const init = () => {
     promptUser()
+        // .then((answers) => generateBadge(answers))
         .then((answers) => writeFileAsync('README.md', generateREADME(answers)))
         .then(() => console.log('Successfully wrote to README.md'))
         .catch((err) => console.error(err));
